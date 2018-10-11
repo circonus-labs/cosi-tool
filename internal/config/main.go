2dc313bfa4de5d4ebce2355cfaf4f3375da3a060
matt maier <maier@users.noreply.github.com>
2018-10-11 14:50:22 -0400

    upd: normalize config settings add: KeyRegConf

--- internal/config/main.go
+++ internal/config/main.go
@@ -20,87 +20,81 @@ import (
 
 // Broker defines the broker and type to use when creating a check
 type Broker struct {
-	ID   string `json:"id" yaml:"id" toml:"id"`
-	Type string `json:"type" yaml:"type" toml:"type"`
+	ID   string `json:"id" toml:"id" yaml:"id"`
+	Type string `json:"type" toml:"type" yaml:"type"`
 }
 
 // Log defines the running config.log structure
 type Log struct {
-	Level  string `json:"level" yaml:"level" toml:"level"`
-	Pretty bool   `json:"pretty" yaml:"pretty" toml:"pretty"`
+	Level  string `json:"level" toml:"level" yaml:"level"`
+	Pretty bool   `json:"pretty" toml:"pretty" yaml:"pretty"`
 }
 
 // API defines the api settings
 type API struct {
-	Key    string `json:"key" yaml:"key" toml:"key"`
-	App    string `json:"app" yaml:"app" toml:"app"`
-	URL    string `json:"url" yaml:"url" toml:"url"`
-	CAFile string `mapstructure:"ca_file" json:"ca_file" yaml:"ca_file" toml:"ca_file"`
+	App    string `json:"app" toml:"app" yaml:"app"`
+	CAFile string `mapstructure:"ca_file" json:"ca_file" toml:"ca_file" yaml:"ca_file"`
+	Key    string `json:"key" toml:"key" yaml:"key"`
+	URL    string `json:"url" toml:"url" yaml:"url"`
 }
 
 // Agent defines the agent settings
 type Agent struct {
-	Mode string `json:"mode" yaml:"mode" toml:"mode"`
-	URL  string `json:"url" yaml:"url" toml:"url"`
+	Mode string `json:"mode" toml:"mode" yaml:"mode"`
+	URL  string `json:"url" toml:"url" yaml:"url"`
 }
 
 // System defines the various system settings
 type System struct {
-	Arch      string `json:"arch" yaml:"arch" toml:"arch"`
-	OSType    string `mapstructure:"os_type" json:"os_type" yaml:"os_type" toml:"os_type"`
-	OSDistro  string `mapstructure:"os_dist" json:"os_dist" yaml:"os_dist" toml:"os_dist"`
-	OSVersion string `mapstructure:"os_vers" json:"os_vers" yaml:"os_vers" toml:"os_vers"`
-	DMI       string `json:"dmi" yaml:"dmi" toml:"dmi"`
+	Arch      string `json:"arch" toml:"arch" yaml:"arch"`
+	DMI       string `json:"dmi" toml:"dmi" yaml:"dmi"`
+	OSDistro  string `mapstructure:"os_dist" json:"os_dist" toml:"os_dist" yaml:"os_dist"`
+	OSType    string `mapstructure:"os_type" json:"os_type" toml:"os_type" yaml:"os_type"`
+	OSVersion string `mapstructure:"os_vers" json:"os_vers" toml:"os_vers" yaml:"os_vers"`
 }
 
 // Host defines the host specific settings
 type Host struct {
-	OptionsFile string `mapstructure:"options_file" json:"options_file" yaml:"options_file" toml:"options_file"`
-	CheckTarget string `mapstructure:"check_target" json:"check_target" yaml:"check_target" toml:"check_target"`
-	GroupID     string `mapstructure:"group_id" json:"group_id" yaml:"group_id" toml:"group_id"`
-	Broker      Broker `json:"broker" yaml:"broker" toml:"broker"`
+	Broker  Broker `json:"broker" toml:"broker" yaml:"broker"`
+	GroupID string `mapstructure:"group_id" json:"group_id" toml:"group_id" yaml:"group_id"`
+	Target  string `json:"target" toml:"target" yaml:"target"`
 }
-
-// Template defines the template specific settings
-// type Template struct {
-// 	List []string `json:"list" yaml:"list" toml:"list"`
-// }
 
 // Config defines the cosi configuration (created by cosi-install or manually)
 type Config struct {
-	CosiURL   string `mapstructure:"cosi_url" json:"cosi_url" yaml:"cosi_url" toml:"cosi_url"`
-	UIBaseURL string `mapstructure:"base_ui_url" json:"base_ui_url" yaml:"base_ui_url" toml:"base_url_ui"`
-	Debug     bool   `json:"debug" yaml:"debug" toml:"debug"`
-	Agent     Agent  `json:"agent" yaml:"agent" toml:"agent"`
-	API       API    `json:"api" yaml:"api" toml:"api"`
-	Host      Host   `json:"host" yaml:"host" toml:"host"`
-	Log       Log    `json:"log" yaml:"log" toml:"log"`
-	System    System `json:"system" yaml:"system" toml:"system"`
-	// Template  Template `json:"template" yaml:"template" toml:"template"`
+	Agent     Agent  `json:"agent" toml:"agent" yaml:"agent"`
+	API       API    `json:"api" toml:"api" yaml:"api"`
+	BaseUIURL string `mapstructure:"base_ui_url" json:"base_ui_url" toml:"base_ui_url" yaml:"base_ui_url"`
+	CosiURL   string `mapstructure:"cosi_url" json:"cosi_url" toml:"cosi_url" yaml:"cosi_url"`
+	Debug     bool   `json:"debug" toml:"debug" yaml:"debug"`
+	Host      Host   `json:"host" toml:"host" yaml:"host"`
+	Log       Log    `json:"log" toml:"log" yaml:"log"`
+	RegConf   string `mapstructure:"reg_conf" json:"reg_conf" toml:"reg_conf" yaml:"reg_conf"`
+	System    System `json:"system" toml:"system" yaml:"system"`
 }
 
-// OldConfig defines the old cosi configuration file format, for conversion
-type OldConfig struct {
-	// backwards compatible options
-	APIKey      string `mapstructure:"api_key" json:"api_key" yaml:"api_key" toml:"api_key"`
-	APIApp      string `mapstructure:"api_app" json:"api_app" yaml:"api_app" toml:"api_app"`
-	APIURL      string `mapstructure:"api_url" json:"api_url" yaml:"api_url" toml:"api_url"`
-	CAFile      string `mapstructure:"api_ca_file" json:"api_ca_file" yaml:"api_ca_file" toml:"api_ca_file"`
-	CosiURL     string `mapstructure:"cosi_url" json:"cosi_url" yaml:"cosi_url" toml:"cosi_url"`
-	AgentMode   string `mapstructure:"agent_mode" json:"agent_mode" yaml:"agent_mode" toml:"agent_mode"`
-	AgentURL    string `mapstructure:"agent_url" json:"agent_url" yaml:"agent_url" toml:"agent_url"`
-	OptsFile    string `mapstructure:"custom_options_file" json:"custom_options_file" yaml:"custom_options_file" toml:"custom_options_file"`
-	CheckTarget string `mapstructure:"cosi_host_target" json:"cosi_host_target" yaml:"cosi_host_target" toml:"cosi_host_target"`
-	BrokerID    string `mapstructure:"cosi_broker_id" json:"cosi_broker_id" yaml:"cosi_broker_id" toml:"cosi_broker_id"`
-	BrokerType  string `mapstructure:"cosi_broker_type" json:"cosi_broker_type" yaml:"cosi_broker_type" toml:"cosi_broker_type"`
-	OSType      string `mapstructure:"cosi_os_type" json:"cosi_os_type" yaml:"cosi_os_type" toml:"cosi_os_type"`
-	OSDistro    string `mapstructure:"cosi_os_dist" json:"cosi_os_dist" yaml:"cosi_os_dist" toml:"cosi_os_dist"`
-	OSVers      string `mapstructure:"cosi_os_vers" json:"cosi_os_vers" yaml:"cosi_os_vers" toml:"cosi_os_vers"`
-	SysArch     string `mapstructure:"cosi_os_arch" json:"cosi_os_arch" yaml:"cosi_os_arch" toml:"cosi_os_arch"`
-	DMI         string `mapstructure:"cosi_os_dmi" json:"cosi_os_dmi" yaml:"cosi_os_dmi" toml:"cosi_os_dmi"`
-	GroupID     string `mapstructure:"cosi_group_id" json:"cosi_group_id" yaml:"cosi_group_id" toml:"cosi_group_id"`
-	UIBaseURL   string `mapstructure:"_ui_base_url" json:"_ui_base_url" yaml:"_ui_base_url" toml:"_ui_base_url"`
-}
+// // OldConfig defines the old cosi configuration file format, for conversion
+// type OldConfig struct {
+// 	// backwards compatible options
+// 	APIKey      string `mapstructure:"api_key" json:"api_key" yaml:"api_key" toml:"api_key"`
+// 	APIApp      string `mapstructure:"api_app" json:"api_app" yaml:"api_app" toml:"api_app"`
+// 	APIURL      string `mapstructure:"api_url" json:"api_url" yaml:"api_url" toml:"api_url"`
+// 	CAFile      string `mapstructure:"api_ca_file" json:"api_ca_file" yaml:"api_ca_file" toml:"api_ca_file"`
+// 	CosiURL     string `mapstructure:"cosi_url" json:"cosi_url" yaml:"cosi_url" toml:"cosi_url"`
+// 	AgentMode   string `mapstructure:"agent_mode" json:"agent_mode" yaml:"agent_mode" toml:"agent_mode"`
+// 	AgentURL    string `mapstructure:"agent_url" json:"agent_url" yaml:"agent_url" toml:"agent_url"`
+// 	OptsFile    string `mapstructure:"custom_options_file" json:"custom_options_file" yaml:"custom_options_file" toml:"custom_options_file"`
+// 	CheckTarget string `mapstructure:"cosi_host_target" json:"cosi_host_target" yaml:"cosi_host_target" toml:"cosi_host_target"`
+// 	BrokerID    string `mapstructure:"cosi_broker_id" json:"cosi_broker_id" yaml:"cosi_broker_id" toml:"cosi_broker_id"`
+// 	BrokerType  string `mapstructure:"cosi_broker_type" json:"cosi_broker_type" yaml:"cosi_broker_type" toml:"cosi_broker_type"`
+// 	OSType      string `mapstructure:"cosi_os_type" json:"cosi_os_type" yaml:"cosi_os_type" toml:"cosi_os_type"`
+// 	OSDistro    string `mapstructure:"cosi_os_dist" json:"cosi_os_dist" yaml:"cosi_os_dist" toml:"cosi_os_dist"`
+// 	OSVers      string `mapstructure:"cosi_os_vers" json:"cosi_os_vers" yaml:"cosi_os_vers" toml:"cosi_os_vers"`
+// 	SysArch     string `mapstructure:"cosi_os_arch" json:"cosi_os_arch" yaml:"cosi_os_arch" toml:"cosi_os_arch"`
+// 	DMI         string `mapstructure:"cosi_os_dmi" json:"cosi_os_dmi" yaml:"cosi_os_dmi" toml:"cosi_os_dmi"`
+// 	GroupID     string `mapstructure:"cosi_group_id" json:"cosi_group_id" yaml:"cosi_group_id" toml:"cosi_group_id"`
+// 	UIBaseURL   string `mapstructure:"_ui_base_url" json:"_ui_base_url" yaml:"_ui_base_url" toml:"_ui_base_url"`
+// }
 
 const (
 	// KeyAPITokenKey circonus api token key
@@ -127,11 +121,11 @@ const (
 	// KeyAgentURL defines the url of the local agent
 	KeyAgentURL = "agent.url"
 
-	// KeyHostOptionsFile defines a json file with custom options
-	KeyHostOptionsFile = "host.options_file"
+	// // KeyHostOptionsFile defines a json file with custom options
+	// KeyHostOptionsFile = "host.options_file"
 
 	// KeyHostTarget defines the target host for the check
-	KeyHostTarget = "host.check_target"
+	KeyHostTarget = "host.target"
 
 	// KeyHostBrokerID defines the broker to use when creating a check
 	KeyHostBrokerID = "host.broker.id"
@@ -158,6 +152,13 @@ const (
 
 	// KeySystemDMI defines the dmi (aws thing)
 	KeySystemDMI = "system.dmi"
+
+	//
+	// Registration
+	//
+
+	// KeyRegConf defines the registration options configuration file
+	KeyRegConf = "register.config"
 
 	//
 	// generic flags
